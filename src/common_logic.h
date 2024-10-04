7d5390e24e029b33bd800f46e2ea2c67335dd426
wilhemiv <wilhemiv@wstudent.21-school.ru>
2024-10-05 02:58:57 +0300

    fix: bugfix

--- src/common_logic.h
+++ src/common_logic.h
@@ -1,6 +1,6 @@
 #ifndef COMMON_LOGIC_H
 #define COMMON_LOGIC_H
 
-void play_round(char *question, char *correct_answer, char *user_name);
+void play_round(const char *question, const char *correct_answer, const char *user_name);
 
 #endif
