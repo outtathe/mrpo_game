38bed13cfd36265562dc72e77b9eca526939982c
wilhemiv <wilhemiv@wstudent.21-school.ru>
2024-10-05 00:57:33 +0300

    feat: final checks for game design

---
+++ src/utils/utils.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "utils.h"
+
+int get_random_number(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+
+int calculate_lcm(int a, int b) {
+    int gcd = calculate_gcd(a, b);
+    return (a * b) / gcd;
+}
+
+int calculate_gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
---
+++ src/utils/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+int get_random_number(int min, int max);
+int calculate_lcm(int a, int b);
+int calculate_gcd(int a, int b);
+
+#endif
