ba798ea649cd3c83a5f5a965e1e30d76166850e5
wilhemiv <wilhemiv@wstudent.21-school.ru>
2024-10-05 00:41:38 +0300

    bug: fix name enter?

--- src/main.c
+++ src/main.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
-#include "common_logic.h"
+
 #include "./games/games.h"
+#include "common_logic.h"
 
 int main() {
-    printf("Welcome to the Brain Games!\n");
-    char user_name[50];
-    printf("May I have your name? ");
-    scanf("%49s", user_name);
-    printf("Hello, %s!\n\n", user_name);
+    printf("+----------------------------------+\n");
+    printf("|    Welcome to the Brain Games!   |\n");
+    printf("+----------------------------------+\n");
+    char user_name[25];
+    printf("|    May I have your name?         |\n");
+    printf("     Enter your name: ");
+    scanf("%24s", user_name);
+    printf("+----------------------------------+\n");
+    // printf("Hello, %s!\n\n", user_name);
 
     int game_choice;
-    printf("Hello, %s, you can play one of these games:\n", user_name);
-    printf("1. Game LCM\n");
-    printf("2. Game Progression\n");
+    printf("  Hello, %s!\n", user_name);
+    printf("| You can play one of these games: |\n");
+    printf("| 1. Game LCM                      |\n");
+    printf("| 2. Game Progression              |\n");
     printf("Please enter the number of the game you want to play: ");
     scanf("%d", &game_choice);
 
     switch (game_choice) {
         case 1:
-            gcd_game(user_name);
+            //   lcm_game(user_name);
+            printf("Okay, you choosed lcm game!\n");
             break;
         case 2:
-            progression_game(user_name);
+            //   progression_game(user_name);
+            printf("Okay, you choosed progression game!\n");
             break;
         default:
-            printf("Invalid choice. Please restart the game and select a valid option.\n");
+            printf(
+                "Invalid choice. Please restart the game and select a valid "
+                "option.\n");
             break;
     }
 
